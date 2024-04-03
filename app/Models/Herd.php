e66eac2114accb6c56cd893e5b484384f7276a59
Erika Vestin <110428412+erbj1201@users.noreply.github.com>
2024-04-03 10:50:32 +0200

    provar ladda upp igen

---
+++ app/Models/Herd.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Herd extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'herdId',
+        'address',
+        'user_id'
+    ];
+    //connect herd to user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //Conntect animal to herd
+    public function animals()
+    {
+        return $this->hasMany(Animal::class);
+    }
+}
