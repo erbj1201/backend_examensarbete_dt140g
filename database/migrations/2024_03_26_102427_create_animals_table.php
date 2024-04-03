e11f2087c9cbfd47a790219fb14503cc0c3832b3
sofiadahlberg <sofiadahlberg@outlook.com>
2024-04-03 10:31:26 +0200

    animals + milk migrations

---
+++ database/migrations/2024_03_26_102427_create_animals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('animalId', 12);
+            $table->string('breed', 20);
+            $table->string('name', 56);
+            $table->bigInteger('herd_id')->unsigned();
+            $table->string('imagepath')->nullable();
+            $table->timestamps();
+            $table->foreign('herd_id')->references('id')->on('herds')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
