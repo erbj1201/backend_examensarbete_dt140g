697ad48516650a3a83b53f4f99af3ee5decd975e
Erika Vestin <erbj1201@student.miun.se>
2024-04-10 09:28:34 +0200

    uppdaterat routes och namn på metoder i controller

--- mingardapi/routes/api.php
+++ mingardapi/routes/api.php
@@ -12,9 +12,9 @@ use Illuminate\Support\Facades\Route;
 /*Routes herds*/
 Route::resource('herds', HerdController::class);
 //Connect herd to user when posting herd
-Route::post ('users/{id}/herds', [UserController::class, 'addHerd']);
+Route::post ('herds/users/{id}', [UserController::class, 'addHerd']);
 //Get all herds for one user
-Route::get ('users/{id}/herds', [UserController::class, 'getHerdsByUser']);
+Route::get ('herds/users/{id}', [UserController::class, 'getHerdsByUser']);
 
 
 /*Routes animals*/
@@ -24,36 +24,49 @@ Route::post('animals/images/{id}', [AnimalController::class, 'updateAnimalImage'
 //Update animal with image
 Route::post('animals/{id}', [AnimalController::class, 'updateAnimalAndImage']);
 //Connect animal to herd when posting animal
-Route::post ('herds/{id}/animals', [HerdController::class, 'addAnimal']);
+Route::post ('animals/herds/{id}', [HerdController::class, 'addAnimal']);
 //Get all animals for one herd
-Route::get ('herds/{id}/animals', [HerdController::class, 'getAnimalsByHerd']);
-//Get all milks from one herd
-Route::get('herds/{id}/milks', [HerdController::class, 'getMilksByHerd']);
+Route::get ('animals/herds/{id}', [HerdController::class, 'getAnimalsByHerd']);
 
 
 /*Routes milk*/
 Route::resource('milks', MilkController::class);
 //Connect milk to animal when posting milk
-Route::post ('animals/{id}/milks', [AnimalController::class, 'addMilk']);
+Route::post ('milks/animals/{id}', [AnimalController::class, 'addMilk']);
 //Get all milks from one animal
-Route::get ('animals/{id}/milks', [AnimalController::class, 'getMilksByAnimal']);
+Route::get ('milks/animals/{id}', [AnimalController::class, 'getMilksByAnimal']);
+//Get all milks from one herd
+Route::get('milks/herds/{id}', [HerdController::class, 'getMilksByHerd']);
 
 /*Routes messages*/
 Route::resource('messages', MessageController::class);
 //Connect message to user when posting message
-Route::post ('users/{id}/messages', [UserController::class, 'addMessage']);
+Route::post ('messages/users/{id}', [UserController::class, 'addMessage']);
 //Get all messages for one user
-Route::get ('users/{id}/messages', [UserController::class, 'getMessagesByUser']);
+Route::get ('messages/users/{id}', [UserController::class, 'getMessagesByUser']);
 //Get all animals for one user
-Route::get('users/{id}/animals', [UserController::class, 'getAnimalsByUser']);
+Route::get('animals/users/{id}', [UserController::class, 'getAnimalsByUser']);
 
 
 /*Route user*/ 
-Route::resource('users', UserController::class);
+//Log in user
+Route::login ('/login', [UserController::class, 'loginUser']);
+//Log out user
+Route::logout('/logout', [UserController::class, 'logoutUser']);
+//Get one user by id
+Route::get('users/{id}', [UserController::class, 'getUserById']);
+//Get all users
+Route::get('users', [UserController::class, 'getAllUsers']);
+//Register new user
+Route::post('register', [UserController::class, 'registerUser']);
+//update user without image
+Route::put ('users/{id}', [UserController::class, 'updateUser']);
 //Update just user image
 Route::post('users/images/{id}', [UserController::class, 'updateUserImage']); 
 //update user with image 
 Route::post('users/{id}', [UserController::class, 'updateUserAndImage']); 
+//Delete user
+Route::delete('users/{id}', [UserController::class, 'destroyUser']); 
 
 /*
 Route::get('/user', function (Request $request) {
